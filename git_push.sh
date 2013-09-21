1c2743000c9c9d9a1908d037b97a6597a63492a5
DevasenaInupakutika <devasena.prasad@gmail.com>
2013-09-21 14:14:21 +0100

    new file uploaded

--- git_push.sh
+++ git_push.sh
@@ -6,19 +6,19 @@ then
    git clone git@10.42.0.95:/home/git/project.git
 else
 
-   echo "Directory already exists" &
-   sudo rm -r /home/turtlebot/fuerte_workspace/sandbox/knowrobcloud/project &
+   echo 'Directory already exists' &
+   rm -r project 
    git clone git@10.42.0.95:/home/git/project.git  
 
 fi
 
 cd project &
 git init &
-sudo cp -r /home/turtlebot/fuerte_workspace/sandbox/knowrobcloud/bagfiles/path_rec.bag /home/turtlebot/fuerte_workspace/sandbox/project/map1.pgm &
+cp -r /home/turtlebot/fuerte_workspace/sandbox/knowrobcloud/bagfiles/path_rec.bag /home/turtlebot/fuerte_workspace/sandbox/project/map1.pgm &
 git add .
-git commit -am 'new file uploaded' &
+git commit -am 'new file uploaded' 
 git remote add origin git@10.42.0.95:/home/git/project.git
-git push origin master &
+git push origin master 
 
 exec "$@"
 
