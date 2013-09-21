1a2e4d55754be9aa59640f5dcf35b982ebbca34b
DevasenaInupakutika <devasena.prasad@gmail.com>
2013-09-21 14:17:48 +0100

    new file uploaded

--- git_push.sh
+++ git_push.sh
@@ -2,7 +2,7 @@
 
 if [! -d /home/turtlebot/fuerte_workspace/sandbox/knowrobcloud/project];
 then
- 
+   echo 'Directory Created' 
    git clone git@10.42.0.95:/home/git/project.git
 else
 
@@ -13,11 +13,9 @@ else
 fi
 
 cd project &
-git init &
 cp -r /home/turtlebot/fuerte_workspace/sandbox/knowrobcloud/bagfiles/path_rec.bag /home/turtlebot/fuerte_workspace/sandbox/project/map1.pgm &
 git add .
 git commit -am 'new file uploaded' 
-git remote add origin git@10.42.0.95:/home/git/project.git
 git push origin master 
 
 exec "$@"
